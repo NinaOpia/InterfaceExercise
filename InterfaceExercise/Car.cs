2346ee6eb608d59726827368cb792563ce02f029
Nina <boldurfaith@gmail.com>
2023-03-22 13:06:28 -0700

    initial commit

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        private bool isChanged;
+
+        public Car()
+        {
+        }
+
+        public double EngineSize { get; set; } = 5.0;
+        public string Make { get; set; } = "Mercedez";
+        public string Model { get; set; } = "C Class";
+        public int SeatCount { get; set; } = 3;
+        public string CompanyName { get; set; } = "Mercedez";
+        public string Motto { get; set; } = "Built strong and reliable";
+        public bool HasChangedGears { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward . . .");
+        }
+
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears");
+            }
+        }
+
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now in park . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't park until we change gears");
+            }
+        }
+
+        public void ChangedGears(bool hasChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+
+        public void ChangeGears(bool isChnaged)
+        {
+            return;
+        }
+    }
+}
+
---
+++ InterfaceExercise/Trunk.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace InterfaceExercise
+{
+	public class Truck : IVehicle, ICompany
+	{
+        private bool isChanged;
+
+        public Truck()
+		{
+		}
+
+        public double EngineSize { get; set; } = 6;
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "F150";
+        public int SeatCount { get; set; } = 3;
+        public string CompanyName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Built strong and reliable";
+        public bool HasChangedGears { get; set; }
+
+        public bool HasFourWheelDrive { get; set; } = true;
+
+        public void Drive()
+        {
+            if (HasFourWheelDrive == true)
+            {
+                Console.WriteLine($"4-Wheel drive {GetType().Name} now driving forward . . .");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} now driving forward . . .");
+            }
+        }
+
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears");
+            }
+        }
+
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now in park . . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't park until we change gears");
+            }
+        }
+
+        public void ChangedGears(bool hasChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+
+        public void ChangeGears(bool isChnaged)
+        {
+            return;
+        }
+    }
+}
+
